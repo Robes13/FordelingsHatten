7865503c1262368b2ef0aff4514c8c4c37332193
Robert <robertorlanderpedersen@gmail.com>
2023-09-13 09:19:50 +0200

    Add project files.

---
+++ Model/SortingHat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FordelingsHatten.Model
+{
+    /// <summary>
+    /// This class is responsible for giving out the faction the student belongs to.
+    /// </summary>
+    internal class SortingHat
+    {
+        public enum Factions
+        {
+            Gryffindor = 1,
+            Hufflepuff,
+            Ravenclaw,
+            Slytherin
+        }
+        public Factions GetFaction(byte factionId)
+        {
+            return (Factions)factionId;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using FordelingsHatten.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FordelingsHatten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingHat sortingHat = new SortingHat();
+            Random getFaction = new Random();
+
+            Console.WriteLine(sortingHat.GetFaction((byte)getFaction.Next(1, 5)));
+        }
+    }
+}
